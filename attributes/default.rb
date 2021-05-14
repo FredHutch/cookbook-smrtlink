60b3550983169f9cb7d844a54814874973f03f34
Michael Gutteridge <michael.gutteridge@gmail.com>
2021-05-14 16:47:32 -0700

    Add service and path information
     - add SMRT_ROOT attribute
     - add systemd unit

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,10 @@ default['smrtlink']['user'] = {
   'home' => '/home/smrtlink',
   'manage_user' => false
 }
+default['smrtlink']['service']['reload'] = false
 # default['smrtlink']['local_dirs'] => {
 #  '/nfs/smrtlink/userdata/db_datadir' => {
 #    'target' => '/var/tmp/smrtlink/db_datadir',
 #    'link' => true
 # }
+default['smrtlink']['paths']['SMRT_ROOT'] = '/home/smrtlink'
--- recipes/default.rb
+++ recipes/default.rb
@@ -23,3 +23,33 @@ if node['smrtlink']['local_dirs']
     end
   end
 end
+
+bash 'daemon-reload' do
+  code '/bin/systemctl daemon-reload'
+  action :nothing
+  only_if { node['smrtlink']['service']['reload'] == true }
+end
+
+systemd_unit 'smrtlink.service' do
+  content(
+    Unit: {
+      Description: 'PacBio SMRTLink Server',
+      After: 'multi-user.target',
+    },
+    Service: {
+      User: node['smrtlink']['user']['username'],
+      Type: 'oneshot',
+      LimitNOFILE: '10240',
+      RemainAfterExit: 'true',
+      EnvironmentFile: '-/etc/default/smrtlink',
+      ExecStart: "#{node['smrtlink']['paths']['SMRT_ROOT']}/admin/bin/services-start",
+      ExecStop: "#{node['smrtlink']['paths']['SMRT_ROOT']}/admin/bin/services-stop",
+    },
+    Install: {
+      WantedBy: 'multi-user.target',
+    }
+  )
+  action [:create]
+  verify false
+  notifies :run, 'bash[daemon-reload]', :delayed
+end
